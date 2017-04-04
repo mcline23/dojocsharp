b7a4d2def66648bd10b32a45182ad6d89317232c
mcline23 <mcline23@hotmail.com>
2017-04-04 12:07:09 -0700

    Human project

---
+++ human/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+
+           public class Human{
+            // Create a base Human class with five attributes: name, strength, intelligence, dexterity, and health.
+            public string name {get; set;}
+
+            public int strength = 3;
+            public int intelligence = 3;
+
+            public int dexterity = 3;
+            public int health = 100;   
+        
+
+        public Human(string nme, int str, int intel, int dex, int hp){
+
+            name = nme;
+            strength = str;
+            intelligence = intel;
+            dexterity = dex;
+            health = hp; 
+        }
+
+        public void Attack(object target){
+            Human dude = target as Human; 
+            if(dude !=null){
+            dude.health -= 5 *strength;
+            }
+         }
+     }
+ }
+                
+            // Console.WriteLine("Hello World!");
+
+
+
+
